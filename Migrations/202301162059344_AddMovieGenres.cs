82996a011f20b68cc56bd858fe9dfa1bc669cc9a
Jonathan <Zweitonegoismus@gmail.com>
2023-01-16 16:23:04 -0500

    Added a genres table and a movies table, along with pages that display all movie details.

---
+++ Migrations/202301162059344_AddMovieGenres.cs
@@ -0,0 +1,21 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieGenres : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Id, Name) VALUES (6, 'Action')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (7, 'Thriller')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (8, 'Family')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (9, 'Romance')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (10, 'Comedy')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Migrations/202301162108200_AddMoviesList.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMoviesList : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Movies (Name, GenreId, DateAdded, ReleaseDate, NumberInStock, NumberAvailable) VALUES ('Iron Giant', 4, 16-1-23, 11-9-1998, 10, 5)");
+            Sql("INSERT INTO Movies (Name, GenreId, DateAdded, ReleaseDate, NumberInStock, NumberAvailable) VALUES ('Spirited Away', 4, 16-1-23, 10-12-1998, 3, 1)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

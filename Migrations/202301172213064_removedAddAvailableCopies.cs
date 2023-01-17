43d9ee7be2edd13ab96b4d58224a76d0dd06a7ec
Jonathan <Zweitonegoismus@gmail.com>
2023-01-17 17:20:33 -0500

    Forms for editing movies and editing user profiles added.

---
+++ Migrations/202301172213064_removedAddAvailableCopies.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class removedAddAvailableCopies : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Movies", "NumberAvailable");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Movies", "NumberAvailable", c => c.Byte(nullable: false));
+        }
+    }
+}

c29c67fe50e7935ffa6b07b80e395d5d4b77210f
Jonathan <Zweitonegoismus@gmail.com>
2023-01-17 18:21:00 -0500

    Added validation to name field on new customer form.

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -29,6 +29,16 @@ namespace Vidly.Controllers
 
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
